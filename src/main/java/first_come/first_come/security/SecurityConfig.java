1493e46eba8b515ecdb82dfc0025e502720ae610
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-21 01:48:18 +0900

    [Feat] SecurityConfig 세션 설정 및 csrf, formlogin, httpbasic 비활성화

--- src/main/java/first_come/first_come/security/SecurityConfig.java
+++ src/main/java/first_come/first_come/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package first_come.first_come.security;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,10 +22,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain FilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().authenticated());
 
 
-
+        // 세션 설정
+        http
+                .sessionManagement((session) ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
