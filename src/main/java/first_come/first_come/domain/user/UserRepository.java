4b0131ff11938926fb98b3f36bb0a6435f7d3b57
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-25 22:04:52 +0900

    [Style] DDD 구조에 따른 패키지 import 수정

--- src/main/java/first_come/first_come/domain/user/UserRepository.java
+++ src/main/java/first_come/first_come/domain/user/UserRepository.java
@@ -1,5 +1,6 @@
 package first_come.first_come.domain.user;
 
+import first_come.first_come.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
