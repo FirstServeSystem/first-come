af970939c54b1a001d02f48d6edc8632642a380e
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-25 22:05:21 +0900

    [Refactor] 이메일 암호화 해제

--- src/main/java/first_come/first_come/domain/user/service/UserServiceImpl.java
+++ src/main/java/first_come/first_come/domain/user/service/UserServiceImpl.java
@@ -1,49 +1,54 @@
 package first_come.first_come.domain.user.service;
 
-import first_come.first_come.domain.mail.EmailServiceImpl;
-import first_come.first_come.domain.user.User;
+import first_come.first_come.domain.mail.EmailService;
 import first_come.first_come.domain.user.UserRepository;
 import first_come.first_come.domain.user.dto.EmailRequestDto;
 import first_come.first_come.domain.user.dto.SignupRequestDto;
 import first_come.first_come.domain.user.dto.SignupResponseDto;
+import first_come.first_come.domain.user.entity.User;
 import first_come.first_come.global.redis.RedisUtil;
 import first_come.first_come.security.EncryptionUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final EncryptionUtils encryptionUtils;
-    private final EmailServiceImpl emailService;
+    private final EmailService emailService;
     private final RedisUtil redisUtil;
 
     @Override
     @Transactional
     public SignupResponseDto userSignup(SignupRequestDto requestDto) {
 
-        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+        String findEmail = encryptionUtils.encrypt(requestDto.getEmail());
+
+        if (userRepository.findByEmail(findEmail).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
 
-        String email = encryptionUtils.encrypt(requestDto.getEmail());
+        emailService.sendEmail(requestDto.getEmail());
+
+//        String email = encryptionUtils.encrypt(requestDto.getEmail());
+        String email = requestDto.getEmail();
         String password = passwordEncoder.encode(requestDto.getPassword());
         String name = encryptionUtils.encrypt(requestDto.getName());
         String phone = encryptionUtils.encrypt(requestDto.getPhone());
         String address = encryptionUtils.encrypt(requestDto.getAddress());
 
-        User user = User.of(email, password,name, phone, address);
-        User saveUser = userRepository.save(user);
+        User user = User.of(email, password, name, phone, address);
+        userRepository.save(user);
 
-        emailService.sendEmail(saveUser);
-
-        return SignupResponseDto.from(saveUser);
+        return SignupResponseDto.from(user);
 
     }
 
@@ -51,10 +56,12 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public boolean verifyEmail(EmailRequestDto requestDto) {
 
+        String value = requestDto.getEmail();
+        String redisCode = redisUtil.getData(value); // 레디스에서 인증코드 받아오기
+
+//        String email = encryptionUtils.encrypt(value);
         String email = requestDto.getEmail();
         String code = requestDto.getCode();
-
-        String redisCode = redisUtil.getData(email);
 
         if (redisCode != null && redisCode.equals(code)) {
             User user = userRepository.findByEmail(email).orElseThrow(
