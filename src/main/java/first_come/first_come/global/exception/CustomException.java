9d6cb52dae646efbdf63ac009fadddf96176cc6d
SUIN <65757661+whtndls@users.noreply.github.com>
2024-09-01 04:25:15 +0900

    [Feat] 커스터 예외처리

---
+++ src/main/java/first_come/first_come/global/exception/CustomException.java
@@ -0,0 +1,13 @@
+package first_come.first_come.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
