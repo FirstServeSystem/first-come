a34f5091da37d4705a391818d931d378f83771e4
SUIN <65757661+whtndls@users.noreply.github.com>
2024-09-01 04:18:51 +0900

    [Refactor] EnableMethodSecurity 어노테이션 제거

--- src/main/java/first_come/first_come/FirstComeApplication.java
+++ src/main/java/first_come/first_come/FirstComeApplication.java
@@ -3,9 +3,11 @@ package first_come.first_come;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableAsync
 @SpringBootApplication
+@EnableMethodSecurity
 public class FirstComeApplication {
 
 	public static void main(String[] args) {
