91dc8e5f3d5a77252c54a6a3ade2ecbee3158aa4
SUIN <65757661+whtndls@users.noreply.github.com>
2024-09-01 04:26:40 +0900

    [Refactor] 출력 에러 메시지

--- src/main/java/first_come/first_come/global/exception/RestApiException.java
+++ src/main/java/first_come/first_come/global/exception/RestApiException.java
@@ -2,11 +2,18 @@ package first_come.first_come.global.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RestApiException {
 
     private int statusCode;
     private String msg;
+
+    public RestApiException(ErrorCode errorCode, String message) {
+        this.statusCode = errorCode.getStatus();
+        this.msg = message;
+    }
 }
