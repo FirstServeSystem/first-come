bf63cd86e6184e8e7534321d56b9bc000ee70acd
SUIN <65757661+whtndls@users.noreply.github.com>
2024-09-01 04:23:32 +0900

    [Feat] exception핸들러

--- src/main/java/first_come/first_come/global/exception/GlobalExceptionHandler.java
+++ src/main/java/first_come/first_come/global/exception/GlobalExceptionHandler.java
@@ -16,4 +16,16 @@ public class GlobalExceptionHandler {
         RestApiException restApiException = new RestApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
         return new ResponseEntity<>(restApiException, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<RestApiException> exception(CustomException ex) {
+        // ErrorCode에서 상태 코드를 받아 사용
+        HttpStatus status = HttpStatus.resolve(ex.getErrorCode().getStatus());
+        if (status == null) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR; // 상태 코드가 잘못되었을 경우 기본값 설정
+        }
+
+        RestApiException restApiException = new RestApiException(ex.getErrorCode(), ex.getMessage());
+        return new ResponseEntity<>(restApiException, status);
+    }
 }
