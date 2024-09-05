112cbadf4b8b732d334d29fd533dbe634620b78a
SUIN <65757661+whtndls@users.noreply.github.com>
2024-09-06 04:47:54 +0900

    [Factor] config 분리

--- src/main/java/first_come/first_come/FirstComeApplication.java
+++ src/main/java/first_come/first_come/FirstComeApplication.java
@@ -2,8 +2,6 @@ package first_come.first_come;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class FirstComeApplication {
